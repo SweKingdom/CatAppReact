e8ad2134ed580f2d30e44401e28aebde1dfd4bab
Philip Oxelius <philip.oxelius@gmail.com>
2026-06-05 11:03:00 +0200

    Add responsive navbar with cart badge and active link highlighting

---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import { Container, NavLink, Nav, Navbar, Badge } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { CartFill } from "react-bootstrap-icons";
+import useCart from '../context/UseCart';
+
+function NavbarComp() {
+    const { totalItems } = useCart();
+    return (
+        <Navbar bg="dark" variant="dark" expand="md" sticky="top">
+            <Container>
+                <Navbar.Brand as={NavLink} to="/" style={{ color: '#ff6600', fontWeight: 700 }}>
+                    Catstore
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-nav" />
+                <Navbar.Collapse id="main-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/cats" end>Cats</Nav.Link>
+                        <Nav.Link as={NavLink} to="/cart" end>
+                            <CartFill className="me-1" />
+                            Cart{' '}
+                            {totalItems > 0 && (
+                                <Badge bg="warning" text="dark" pill>{totalItems}</Badge>
+                            )}
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    ) 
+}
+
+export default NavbarComp;
